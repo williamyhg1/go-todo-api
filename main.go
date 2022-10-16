648baf05e7779bc2a64558c974077b2d40820dd6
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-10-16 18:35:15 +1100

    finish api

--- main.go
+++ main.go
@@ -4,13 +4,29 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Clean Room", Completed: false},
+	{ID: "2", Item: "Read Book", Completed: false},
+	{ID: "3", Item: "Record Video", Completed: false},
+}
+
+func getTodos(context *gin.Context){
+	context.IndentedJSON(http.StatusOK, todos)
+}
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-	  c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	  })
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-  }
+	// r.GET("/todos", getTodos)
+	// r.Run()
+
+  r.GET("/todos", func(c *gin.Context) {
+    c.JSON(http.StatusOK, todos)
+  })
+  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080"
+}
